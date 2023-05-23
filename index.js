c5ec9b10651f8ec6a6a31407eb5f2e3923c9d344
Eunbyul <las4134@gmail.com>
2023-05-23 07:57:17 +0100

    Add hamburger button navigation, media query, JS

---
+++ index.js
@@ -0,0 +1,27 @@
+const toggleBar = document.querySelector('.fa-bars')
+const navSection = document.querySelector('.toogle-nav-section')
+
+const navLink = document.querySelectorAll('.toggle-nav-link')
+
+
+navLink.forEach( (link) => {
+    link.addEventListener("click", function(){
+        navSection.style.height = '1px' 
+    })
+})
+
+
+const displayNav = () => {
+    if(navSection.style.height === '1px'){
+        navSection.style.height = '170px' 
+    }else{
+        navSection.style.height = '1px'   
+    }
+
+}
+
+toggleBar.addEventListener("click", displayNav)
+
+
+
+
