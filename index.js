bbf4c4d791bd3d13e6445b50ceaaa892c870a898
Eunbyul <las4134@gmail.com>
2023-05-29 21:49:05 +0100

    modify tabindex, navbar css, modify navbar js

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
-const toggleBar = document.querySelector('.fa-bars')
-const navSection = document.querySelector('.togle-nav-section')
-const navLink = document.querySelectorAll('.toggle-nav-link')
-let w = window.innerWidth
+const toggleBar = document.querySelector('.fa-bars');
+const navSection = document.querySelector('.togle-nav-section');
+const navLink = document.querySelectorAll('.toggle-nav-link');
+let w = window.innerWidth;
 
 
 navLink.forEach( (link) => {
     link.addEventListener("click", function(){
-        navSection.style.height = '1px' 
+        navSection.style.visibility = "hidden";
+        navSection.style.height = '1px';
     })
 })
 
 
 const displayNav = () => {
-    if(navSection.style.height === '1px'){
-        navSection.style.height = '170px' 
+    if(navSection.style.visibility === "hidden" && navSection.style.height === '1px'){
+        navSection.style.visibility = "visible";
+        navSection.style.height = '170px';
     }else{
-        navSection.style.height = '1px'   
+        navSection.style.visibility = "hidden";
+        navSection.style.height = '1px'; 
     }
 }
 
-toggleBar.addEventListener("click", displayNav)
-window.addEventListener('resize', resizing)
+toggleBar.addEventListener("click", displayNav);
+window.addEventListener('resize', resizing);
 
 
-function resizing(){   
+function resizing(){  
     if(w > 768){
-        navSection.style.height = '1px'  
+        navSection.style.visibility = "hidden";
+        navSection.style.height = '1px';
     }
 }
-
-
-
-
-
-
-
-
-
 
 
